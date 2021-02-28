83fff7f1710e13271bb3a9a221dccba583cadda2
Suily77 <pengsulong@foxemail.com>
2021-02-28 17:07:32 +0800

    swagger配置类

---
+++ src/main/java/com/psl/sprigbootdemo/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.psl.sprigbootdemo.config;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().build();
+    }
+}
